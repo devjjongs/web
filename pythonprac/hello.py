92600d0f59352d3cf8b2ed5e6e9ce7c76f57b49d
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-04-23 16:31:03 +0900

    Update hello.py
    네이버 영화에서 영화 순위, 이름, 평점 데이터 크롤링 연습

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -67,12 +67,18 @@ soup = BeautifulSoup(data.text, 'html.parser')
 # print(soup)
 # #old_content > table > tbody > tr:nth-child(3) > td.title > div > a
 # #old_content > table > tbody > tr:nth-child(4) > td.title > div > a
+# #old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
+# #old_content > table > tbody > tr:nth-child(2) > td.point
 
 movies = soup.select('#old_content > table > tbody > tr')
 # print(movies)
 
 for movie in movies:
-    a = movie.select_one('td.title > div > a')
-    # print(a)
-    if a is not None:
-        print(a.text)
+    rank = movie.select_one('td:nth-child(1) > img')
+    # print(rank['alt'])
+    title = movie.select_one('td.title > div > a')
+    # print(title.text)
+    star = movie.select_one('td.point')
+    # print(star.text)
+    if title is not None:
+        print(rank['alt'], title.text, star.text)

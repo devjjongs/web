a7ac194ccc73743e6950a765e901e6ceddca65bd
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-04-24 15:47:30 +0900

    practice GET/POST API
    GET/POST API를 이용하는 방법 연습

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def home ():
     return render_template('index.html')
+
+
+@app.route('/test', methods=['GET'])
+def test_get ():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
+@app.route('/test', methods=['POST'])
+def test_post ():
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청을 잘 받았어요'})
 
 
 if __name__ == '__main__':

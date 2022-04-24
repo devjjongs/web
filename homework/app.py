7ae5f641718230be27386afb66814b51ae4de768
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-04-24 23:26:48 +0900

    practice POST/GET API
    POST/GET API 와 ajax를 이용하여 응원 코멘트를 작성한 후 저장하고, 저장된 응원글을 불러와 ajax를 이용하여 출력

--- homework/app.py
+++ homework/app.py
@@ -1,19 +1,41 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.zuy17.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+db = client.dbsparta
+
+
 @app.route('/')
-def home():
+def home ():
     return render_template('index.html')
 
+
 @app.route("/homework", methods=["POST"])
-def homework_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결 완료!'})
+def homework_post ():
+    nickname_receive = request.form['nickname_give']
+    comment_receive = request.form['comment_give']
+    # print(sample_receive)
+
+    doc = {
+        'nickname': nickname_receive,
+        'comment': comment_receive
+    }
+    db.cheering.insert_one(doc)
+
+    return jsonify({'msg': '응원 완료!'})
+
 
 @app.route("/homework", methods=["GET"])
-def homework_get():
-    return jsonify({'msg':'GET 연결 완료!'})
+def homework_get ():
+    all_comment = list(db.cheering.find({}, {'_id': False}))
+    return jsonify({'comments': all_comment})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
